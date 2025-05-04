5cba0df5caef8eb0f3807f8d2f2e9d818d5ff31b
faliszewskii <igor.faliszewski1@gmail.com>
2025-05-04 21:22:01 +0200

    Move frame generator to C

--- frame_generator.py
+++ frame_generator.py
@@ -1,121 +1,48 @@
 import math
 
+import ctypes
 import numpy as np
-from scipy.interpolate import barycentric_interpolate
+import os
 
 
 class FrameGenerator:
     def __init__(self):
-        self.tetrahedra = [
-            [[0, 1, 1], [1, 1, 1], [0, 1, 0], [0, 0, 1]],
-            [[0, 0, 1], [1, 0, 1], [1, 0, 0], [1, 1, 1]],
-            [[1, 1, 1], [1, 0, 0], [1, 1, 0], [0, 1, 0]],
-            [[0, 0, 1], [1, 0, 0], [0, 0, 0], [0, 1, 0]],
-            [[1, 1, 1], [0, 1, 0], [1, 0, 0], [0, 0, 1]]
+        pass
+
+    def generateFrames(self, startFrame, totalFrameCount, transformType, isCyclical):
+        framesToGenerate = totalFrameCount - 1
+        if isCyclical:
+            framesToGenerate -= 1
+
+        # lib_path = os.path.abspath('./libframe_generator.dll')  # TODO Returns 'C:\Users\USER\libframe_generator.dll'
+        lib_path = f'C:\\Users\\USER\\Documents\\Repositories\\temporal-voxel-tracking\\libframe_generator.dll'
+        lib = ctypes.CDLL(lib_path)
+
+        lib.generateFrames.argtypes = [
+            ctypes.POINTER(ctypes.c_float),  # Input frame
+            ctypes.POINTER(ctypes.c_float),  # Output frames
+            ctypes.c_int, ctypes.c_int, ctypes.c_int,  # Dimensions
+            ctypes.c_int,  # Frame count
+            ctypes.c_int,  # Transform function enum
+            ctypes.c_bool,  # Is cyclical
         ]
+        lib.generateFrames.restype = None
 
-    def generateFrames(self, startFrame, totalFrameCount, transform, isCyclical):
-        frames = [startFrame]
-
-        count = totalFrameCount
-        if isCyclical:
-            count -= 1
-
-        for t in range(1, count):
-            frame = self.generateFrame(startFrame, transform, t, totalFrameCount)
-            frames.append(frame)
-
-        if isCyclical:
-            frames.append(startFrame)
-
-        return frames
-
-    def generateFrame(self, startFrame, transform, t, totalFrameCount):
         dim = startFrame.shape
-        print(f"Frame {t}")
+        transformedFrame = np.empty((framesToGenerate, dim[0], dim[1], dim[2]), dtype=np.float32, order='C')
 
-        transformedCoordinates = np.empty([dim[0], dim[1], dim[2], 3])
-        for nx in range(dim[0]):
-            for ny in range(dim[1]):
-                for nz in range(dim[2]):
-                    transformedCoordinates[nx, ny, nz, :] = transform(np.array([nx, ny, nz]), t, np.array([dim[0], dim[1], dim[2]]), totalFrameCount)
+        start_ptr = startFrame.reshape(dim, order='C').flatten().ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+        transformed_ptr = transformedFrame.flatten().ctypes.data_as(ctypes.POINTER(ctypes.c_float))
 
-        transformedFrame = np.empty(dim)
-        for nx in range(dim[0]-1):
-            print(f"[{nx}, :, :]")
-            for ny in range(dim[1]-1):
-                for nz in range(dim[2]-1):
-                    # For each tetrahedron get its bounding box and try to assign values to the integer numbered cells
-                    # that fit inside the bounding box
-                    for tetra in self.tetrahedra:
-                        p0 = transformedCoordinates[nx + tetra[0][0], ny + tetra[0][1], nz + tetra[0][2], :]
-                        p1 = transformedCoordinates[nx + tetra[1][0], ny + tetra[1][1], nz + tetra[1][2], :]
-                        p2 = transformedCoordinates[nx + tetra[2][0], ny + tetra[2][1], nz + tetra[2][2], :]
-                        p3 = transformedCoordinates[nx + tetra[3][0], ny + tetra[3][1], nz + tetra[3][2], :]
-                        weights = [
-                            startFrame[nx + tetra[0][0], ny + tetra[0][1], nz + tetra[0][2]],
-                            startFrame[nx + tetra[1][0], ny + tetra[1][1], nz + tetra[1][2]],
-                            startFrame[nx + tetra[2][0], ny + tetra[2][1], nz + tetra[2][2]],
-                            startFrame[nx + tetra[3][0], ny + tetra[3][1], nz + tetra[3][2]]
-                        ]
+        lib.generateFrames(start_ptr, transformed_ptr, dim[0], dim[1], dim[2], totalFrameCount, transformType, isCyclical)
 
-                        if weights[0] == weights[1] == weights[2] == weights[3] == 0:
-                            continue
+        transformedFrame = np.ctypeslib.as_array(transformed_ptr, shape=(framesToGenerate * dim[0] * dim[1] * dim[2],))
+        transformedFrame = transformedFrame.reshape((framesToGenerate, dim[0], dim[1], dim[2]), order='C').astype(np.float32)
 
-                        (minX, maxX, minY, maxY, minZ, maxZ) = self.getBoundingBox(p0, p1, p2, p3)
+        outputFrames = [transformedFrame[i] for i in range(transformedFrame.shape[0])]
 
-                        vab = p1 - p0
-                        vac = p2 - p0
-                        vad = p3 - p0
-                        vbc = p2 - p1
-                        vbd = p3 - p1
-                        bd_bc = np.cross(vbd, vbc)
-                        ac_ad = np.cross(vac, vad)
-                        ad_ab = np.cross(vad, vab)
-                        ab_ac = np.cross(vab, vac)
-                        v6 = 1 / np.dot(vab, ac_ad)
-                        for bx in range(max(0, minX), min(maxX + 1, dim[0])):
-                            for by in range(max(0, minY), min(maxY + 1, dim[1])):
-                                for bz in range(max(0, minZ), min(maxZ + 1, dim[2])):
-                                    p = np.array([bx, by, bz])
-                                    vap = p - p0
-                                    vbp = p - p1
+        outputFrames = [startFrame] + outputFrames
+        if isCyclical:
+            outputFrames = outputFrames + [startFrame]
 
-                                    va = np.dot(vbp, bd_bc) * v6
-                                    vb = np.dot(vap, ac_ad) * v6
-                                    vc = np.dot(vap, ad_ab) * v6
-                                    vd = np.dot(vap, ab_ac) * v6
-
-                                    outside = va < 0 or vb < 0 or vc < 0 or vd < 0
-
-                                    w = va * weights[0] + vb * weights[1] + vc * weights[2] + vd * weights[3]
-
-                                    if not outside:
-                                        transformedFrame[bx, by, bz] = w
-        return transformedFrame
-
-    def getBoundingBox(self, p0, p1, p2, p3):
-        minX = int(math.ceil(min(p0[0], p1[0], p2[0], p3[0])))
-        minY = int(math.ceil(min(p0[1], p1[1], p2[1], p3[1])))
-        minZ = int(math.ceil(min(p0[2], p1[2], p2[2], p3[2])))
-        maxX = int(math.floor(max(p0[0], p1[0], p2[0], p3[0])))
-        maxY = int(math.floor(max(p0[1], p1[1], p2[1], p3[1])))
-        maxZ = int(math.floor(max(p0[2], p1[2], p2[2], p3[2])))
-
-        return minX, maxX, minY, maxY, minZ, maxZ
-
-    # def barycentricInterpolation(self, points, weights, p, vab, vac, vad, vbc, vbd, v6):
-    #     vap = p - points[0]
-    #     vbp = p - points[1]
-    #
-    #     va6 = np.dot( )self.sctp(vbp, vbd, vbc) * v6
-    #     vb6 = self.sctp(vap, vac, vad) * v6
-    #     vc6 = self.sctp(vap, vad, vab) * v6
-    #     vd6 = self.sctp(vap, vab, vac) * v6
-    #
-    #     outside = va6 < 0 or vb6 < 0 or vc6 < 0 or vd6 < 0
-    #
-    #     return va6 * weights[0] + vb6 * weights[1] + vc6 * weights[2] + vd6 * weights[3], outside
-
-    # def sctp(self, a, b, c):
-    #     return np.dot(a, np.cross(b, c))
+        return outputFrames
